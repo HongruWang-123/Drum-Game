d6db69ba8a69712bf276a13ee431db1e65899a46
Hongru Wang <74997670+HongruWang-123@users.noreply.github.com>
2022-06-25 16:43:30 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+function addAnime(key){
+  document.querySelector("." + key).classList.add("pressed");
+  setTimeout(function(){
+    document.querySelector("." + key).classList.remove("pressed");
+  },100)
+}
+
+document.addEventListener("keypress",function(event){
+  checkkey(event.key);
+  addAnime(event.key);
+});
+
+for (var i = 0; i <= document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+    var buttonclicked = this.textContent;
+    checkkey(buttonclicked);
+    addAnime(buttonclicked);
+  });
+}
+
+function checkkey(key){
+  switch (key) {
+    case "w":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+  
+  default: console.log(key);
+}
+}
